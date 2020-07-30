2e192750f339faf1d71c23de91b1908cffac28e2
Hossam <hossamsaeedg99@yahoo.com>
2020-07-30 22:16:50 +0200

    js event & email validation

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var btn = document.querySelector('#button') ;
+
+var entered = document.querySelector('#email');
+var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ;
+var errorImg = document.getElementById('error-image');
+var errorPlace = document.getElementById('error-place');
+var form = document.querySelector('form');
+
+function check (){
+    if ( !entered.value.match(emailFormat) || entered.value == "" ){
+        entered.style.border= "1px solid #ff0000";
+        errorImg.style.visibility = "visible";
+        errorPlace.innerHTML= "Please provide a valid email";
+        errorPlace.style.color = "#ff0000"; 
+
+    }else {
+        entered.style.border= "none";
+        errorImg.style.visibility = "hidden";
+        errorPlace.innerHTML= "";
+
+    };    
+};
+
+
+btn.addEventListener('click', function (event) {
+    check();  
+} );
+
+
+
+
+
+    
